bf36239a6ce003d725653209649b91f88ad2982f
Nicolas Bovorasmy <nicolas.bovorasmy@gmail.com>
2017-03-25 18:11:23 +0100

    Add Link between Local, FB & Google accounts

--- config/passport.js
+++ config/passport.js
@@ -32,8 +32,9 @@ module.exports = function(passport) {
 				if(err)
 					return done(err);
 				if(user){
-					return done(null, false, req.flash('signupMessage', 'This email has been already taken'));
-				} else {
+					return done(null, false, req.flash('signupMessage', 'That email already taken'));
+				} 
+				if(!req.user) {
 					var newUser = new User();
 					newUser.local.username = email;
 					newUser.local.password = newUser.generateHash(password);
@@ -43,11 +44,22 @@ module.exports = function(passport) {
 							throw err;
 						return done(null, newUser);
 					})
+				} else {
+					var user = req.user;
+					user.local.username = email;
+					user.local.password = user.generateHash(password);
+
+					user.save(function(err){
+						if(err)
+							throw err;
+						return done(null, user);
+					})
 				}
 			})
 
 		});
 	}));
+
 
 	passport.use('local-login', new LocalStrategy({
 			usernameField: 'email',
@@ -74,64 +86,97 @@ module.exports = function(passport) {
 passport.use(new FacebookStrategy({
 	    clientID: configAuth.facebookAuth.clientID,
 	    clientSecret: configAuth.facebookAuth.clientSecret,
-	    callbackURL: configAuth.facebookAuth.callbackURL
+	    callbackURL: configAuth.facebookAuth.callbackURL,
+	    passReqToCallback: true
 	  },
-	  function(accessToken, refreshToken, profile, done) {
+	  function(req, accessToken, refreshToken, profile, done) {
 	    	process.nextTick(function(){
-	    		User.findOne({'facebook.id': profile.id}, function(err, user){
-	    			console.log(profile);
+	    		//user is not logged in yet
+	    		if(!req.user){
+					User.findOne({'facebook.id': profile.id}, function(err, user){
+		    			if(err)
+		    				return done(err);
+		    			if(user)
+		    				return done(null, user);
+		    			else {
+		    				var newUser = new User();
+		    				newUser.facebook.id = profile.id;
+		    				newUser.facebook.token = accessToken;
+		    				newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+		    				//newUser.facebook.email = profile.emails[0].value;
 
-	    			if(err)
-	    				return done(err);
-	    			if(user)
+		    				newUser.save(function(err){
+		    					if(err)
+		    						throw err;
+		    					return done(null, newUser);
+		    				})
+		    			}
+		    		});
+	    		}
+
+	    		//user is logged in already, and needs to be merged
+	    		else {
+	    			var user = req.user;
+	    			user.facebook.id = profile.id;
+	    			user.facebook.token = accessToken;
+	    			user.facebook.name = profile.displayName;
+	    			//user.facebook.email = profile.emails[0].value;
+
+	    			user.save(function(err){
+	    				if(err)
+	    					throw err
 	    				return done(null, user);
-	    			else {
-	    				var newUser = new User();
-	    				newUser.facebook.id = profile.id;
-	    				newUser.facebook.token = accessToken;
-	    				newUser.facebook.name = profile.displayName;
-	    			
-
-	    				newUser.save(function(err){
-	    					if(err)
-	    						throw err;
-	    					return done(null, newUser);
-	    				})
-	    				
-	    			}
-	    		});
+	    			})
+	    		}
+	    		
 	    	});
 	    }
 
 	));
 
-passport.use(new GoogleStrategy({
+	passport.use(new GoogleStrategy({
 	    clientID: configAuth.googleAuth.clientID,
 	    clientSecret: configAuth.googleAuth.clientSecret,
-	    callbackURL: configAuth.googleAuth.callbackURL
+	    callbackURL: configAuth.googleAuth.callbackURL,
+	    passReqToCallback: true
 	  },
-	  function(accessToken, refreshToken, profile, done) {
+	  function(req, accessToken, refreshToken, profile, done) {
 	    	process.nextTick(function(){
-	    		User.findOne({'google.id': profile.id}, function(err, user){
-	    			if(err)
-	    				return done(err);
-	    			if(user)
-	    				return done(null, user);
-	    			else {
-	    				var newUser = new User();
-	    				newUser.google.id = profile.id;
-	    				newUser.google.token = accessToken;
-	    				newUser.google.name = profile.displayName;
-	    				newUser.google.email = profile.emails[0].value;
 
-	    				newUser.save(function(err){
-	    					if(err)
-	    						throw err;
-	    					return done(null, newUser);
-	    				})
-	    				console.log(profile);
-	    			}
-	    		});
+	    		if(!req.user){
+	    			User.findOne({'google.id': profile.id}, function(err, user){
+		    			if(err)
+		    				return done(err);
+		    			if(user)
+		    				return done(null, user);
+		    			else {
+		    				var newUser = new User();
+		    				newUser.google.id = profile.id;
+		    				newUser.google.token = accessToken;
+		    				newUser.google.name = profile.displayName;
+		    				newUser.google.email = profile.emails[0].value;
+
+		    				newUser.save(function(err){
+		    					if(err)
+		    						throw err;
+		    					return done(null, newUser);
+		    				})
+		    			}
+		    		});
+	    		} else {
+	    			var user = req.user;
+	    			user.google.id = profile.id;
+					user.google.token = accessToken;
+					user.google.name = profile.displayName;
+					user.google.email = profile.emails[0].value;
+
+					user.save(function(err){
+						if(err)
+							throw err;
+						return done(null, user);
+					});
+	    		}
+	    		
 	    	});
 	    }
 
