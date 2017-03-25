50a079940d809ba25d2c35ba4ca3b2499b37637a
Nicolas Bovorasmy <nicolas.bovorasmy@gmail.com>
2017-03-25 14:07:15 +0100

    Add Facebook Auth

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,10 @@
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 
 var User = require('../app/models/user');
+
+var configAuth = require('./auth');
 
 module.exports = function(passport) {
 
@@ -65,6 +68,40 @@ module.exports = function(passport) {
 				});
 			});
 		}
+	));
+
+passport.use(new FacebookStrategy({
+	    clientID: configAuth.facebookAuth.clientID,
+	    clientSecret: configAuth.facebookAuth.clientSecret,
+	    callbackURL: configAuth.facebookAuth.callbackURL
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+	    	process.nextTick(function(){
+	    		User.findOne({'facebook.id': profile.id}, function(err, user){
+	    			console.log(profile);
+
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    				return done(null, user);
+	    			else {
+	    				var newUser = new User();
+	    				newUser.facebook.id = profile.id;
+	    				newUser.facebook.token = accessToken;
+	    				newUser.facebook.name = profile.displayName;
+	    			
+
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
+	    				
+	    			}
+	    		});
+	    	});
+	    }
+
 	));
 
 
